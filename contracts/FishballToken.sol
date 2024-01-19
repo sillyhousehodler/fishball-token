4754c64d7550ee337511c9bd2537db1ba4146d66
sillymaster <sillyhouse.xyz@gmail.com>
2024-01-19 16:38:06 +0800

    first contract updated

---
+++ contracts/FishballToken.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract FishballToken is ERC20 {
+    constructor(uint initialSupply) ERC20("FishballToken", "FBT") {
+        _mint(msg.sender, initialSupply);
+    }
+}
