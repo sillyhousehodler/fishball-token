53450d0d53a6bc5718b14fb5123f82bc6c7fa0bd
sillymaster <126859077+sillyhousehodler@users.noreply.github.com>
2024-01-19 23:46:06 +0800

    Comment test from github

--- contracts/FishballToken.sol
+++ contracts/FishballToken.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+// This is comment test
 contract FishballToken is ERC20 {
     constructor(uint initialSupply) ERC20("FishballToken", "FBT") {
         _mint(msg.sender, initialSupply);
