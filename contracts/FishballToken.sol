ba3a4a348c8ccdaf27dd6e62d601b07227b3c854
sillymaster <126859077+sillyhousehodler@users.noreply.github.com>
2024-02-04 23:16:33 +0800

    Errors update

--- contracts/FishballToken.sol
+++ contracts/FishballToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
@@ -10,12 +10,9 @@ contract FishballToken is ERC20Capped, ERC20Burnable {
     address payable public owner;
     uint256 public blockReward;
 
-    constructor(
-        uint256 cap,
-        uint256 reward
-    ) ERC20("FishballToken", "FBT") ERC20Capped(cap * (10 ** decimals())) {
+    constructor(uint256 cap, uint256 reward) ERC20("FishballToken", "FBT") ERC20Capped(cap * (10 ** decimals())) {
         owner = msg.sender;
-        _mint(owner, 7000000 * (10 ** decimals()));
+        _mint(owner, 70000000 * (10 ** decimals()));
         blockReward = reward * (10 ** decimals());
     }
 
@@ -23,16 +20,9 @@ contract FishballToken is ERC20Capped, ERC20Burnable {
         _mint(block.coinbase, blockReward);
     }
 
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 value
-    ) internal virtual override {
-        if (
-            from != address(0) &&
-            to != block.coinbase &&
-            block.coinbase != address(0)
-        ) {
+    function _beforeTokenTransfer(address from, address to, uint256 value) internal virtual override {
+        if (from != address(0) && to != block.coinbase && block.coinbase != address(0))
+        {
             _mintMinerReward();
         }
         super._beforeTokenTransfer(from, to, value);
